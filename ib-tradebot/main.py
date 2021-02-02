c1a5354d290038b2088f905a217b913cc609cbc5
Siegfried Eckstedt <39965395+siegstedt@users.noreply.github.com>
2021-02-02 15:55:52 +0100

    adding back testing

--- ib-tradebot/main.py
+++ ib-tradebot/main.py
@@ -18,6 +18,15 @@ class TradingApp(EWrapper, EClient):
         """Initiate TradingApp by inheriting the Eclient class"""
         EClient.__init__(self, self)
         self.data = {}
+        self.order_df = pd.DataFrame(columns=['PermId', 'ClientId', 'OrderId',
+                                              'Account', 'Symbol', 'SecType',
+                                              'Exchange', 'Action', 'OrderType',
+                                              'TotalQty', 'CashQty', 'LmtPrice',
+                                              'AuxPrice', 'Status'])
+        self.pos_df = pd.DataFrame(columns=['Account', 'Symbol', 'SecType',
+                                            'Currency', 'Position', 'Avg cost'])
+        self.acc_summary = pd.DataFrame(columns=['ReqId', 'Account', 'Tag', 'Value', 'Currency'])
+        self.pnl_summary = pd.DataFrame(columns=['ReqId', 'DailyPnL', 'UnrealizedPnL', 'RealizedPnL'])
 
     def error(self, reqId, errorCode, errorString):
         print(
@@ -59,6 +68,35 @@ class TradingApp(EWrapper, EClient):
         super().nextValidId(orderId)
         self.nextValidOrderId = orderId
         print("NextValidId:", orderId)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        """retrieve open orders into a dataframe"""
+        super().openOrder(orderId, contract, order, orderState)
+        dictionary = {"PermId":order.permId, "ClientId": order.clientId, "OrderId": orderId, 
+                      "Account": order.account, "Symbol": contract.symbol, "SecType": contract.secType,
+                      "Exchange": contract.exchange, "Action": order.action, "OrderType": order.orderType,
+                      "TotalQty": order.totalQuantity, "CashQty": order.cashQty, 
+                      "LmtPrice": order.lmtPrice, "AuxPrice": order.auxPrice, "Status": orderState.status}
+        self.order_df = self.order_df.append(dictionary, ignore_index=True)
+
+    def position(self, account, contract, position, avgCost):
+        """retrieve all current positions to a dataframe"""
+        super().position(account, contract, position, avgCost)
+        dictionary = {"Account":account, "Symbol": contract.symbol, "SecType": contract.secType,
+                      "Currency": contract.currency, "Position": position, "Avg cost": avgCost}
+        self.pos_df = self.pos_df.append(dictionary, ignore_index=True)
+
+    def accountSummary(self, reqId, account, tag, value, currency):
+        """retrieve an account summary to a dataframe"""
+        super().accountSummary(reqId, account, tag, value, currency)
+        dictionary = {"ReqId":reqId, "Account": account, "Tag": tag, "Value": value, "Currency": currency}
+        self.acc_summary = self.acc_summary.append(dictionary, ignore_index=True)
+        
+    def pnl(self, reqId, dailyPnL, unrealizedPnL, realizedPnL):
+        """retrieve a profit and loss summary to a dataframe"""
+        super().pnl(reqId, dailyPnL, unrealizedPnL, realizedPnL)
+        dictionary = {"ReqId":reqId, "DailyPnL": dailyPnL, "UnrealizedPnL": unrealizedPnL, "RealizedPnL": realizedPnL}
+        self.pnl_summary = self.pnl_summary.append(dictionary, ignore_index=True)        
 
 
 def websocket_con():
@@ -180,6 +218,126 @@ def trailStopOrder(direction, quantity, st_price, tr_step=1):
     return order
 
 
+def getOpenOrders():
+    app.reqOpenOrders()
+    time.sleep(1)
+    order_df = app.order_df
+    return order_df
+
+def getCurrentPositions():
+    app.reqPositions()
+    time.sleep(1)
+    pos_df = app.pos_df
+    return pos_df
+
+def getAccountSummary():
+    app.reqAccountSummary(1, "All", "$LEDGER:ALL")
+    time.sleep(1)
+    acc_summ_df = app.acc_summary
+    return acc_summ_df
+
+def getPnlSummary():
+    app.reqPnL(2, "DU3213143", "")
+    time.sleep(1)
+    pnl_summ_df = app.pnl_summary
+    return pnl_summ_df
+
+def MACD(DF,a=12,b=26,c=9):
+    """function to calculate MACD
+       typical values a(fast moving average) = 12; 
+                      b(slow moving average) =26; 
+                      c(signal line ma window) =9"""
+    df = DF.copy()
+    df["MA_Fast"]=df["Close"].ewm(span=a,min_periods=a).mean()
+    df["MA_Slow"]=df["Close"].ewm(span=b,min_periods=b).mean()
+    df["MACD"]=df["MA_Fast"]-df["MA_Slow"]
+    df["Signal"]=df["MACD"].ewm(span=c,min_periods=c).mean()
+    df.dropna(inplace=True)
+    return df
+
+def bollBnd(DF,n=20):
+    "function to calculate Bollinger Band"
+    df = DF.copy()
+    #df["MA"] = df['close'].rolling(n).mean()
+    df["MA"] = df['Close'].ewm(span=n,min_periods=n).mean()
+    df["BB_up"] = df["MA"] + 2*df['Close'].rolling(n).std(ddof=0) #ddof=0 is required since we want to take the standard deviation of the population and not sample
+    df["BB_dn"] = df["MA"] - 2*df['Close'].rolling(n).std(ddof=0) #ddof=0 is required since we want to take the standard deviation of the population and not sample
+    df["BB_width"] = df["BB_up"] - df["BB_dn"]
+    df.dropna(inplace=True)
+    return df
+
+def atr(DF,n):
+    "function to calculate True Range and Average True Range"
+    df = DF.copy()
+    df['H-L']=abs(df['High']-df['Low'])
+    df['H-PC']=abs(df['High']-df['Close'].shift(1))
+    df['L-PC']=abs(df['Low']-df['Close'].shift(1))
+    df['TR']=df[['H-L','H-PC','L-PC']].max(axis=1,skipna=False)
+    #df['ATR'] = df['TR'].rolling(n).mean()
+    df['ATR'] = df['TR'].ewm(com=n,min_periods=n).mean()
+    return df['ATR']
+
+def rsi(DF,n=20):
+    "function to calculate RSI"
+    df = DF.copy()
+    df['delta']=df['Close'] - df['Close'].shift(1)
+    df['gain']=np.where(df['delta']>=0,df['delta'],0)
+    df['loss']=np.where(df['delta']<0,abs(df['delta']),0)
+    avg_gain = []
+    avg_loss = []
+    gain = df['gain'].tolist()
+    loss = df['loss'].tolist()
+    for i in range(len(df)):
+        if i < n:
+            avg_gain.append(np.NaN)
+            avg_loss.append(np.NaN)
+        elif i == n:
+            avg_gain.append(df['gain'].rolling(n).mean()[n])
+            avg_loss.append(df['loss'].rolling(n).mean()[n])
+        elif i > n:
+            avg_gain.append(((n-1)*avg_gain[i-1] + gain[i])/n)
+            avg_loss.append(((n-1)*avg_loss[i-1] + loss[i])/n)
+    df['avg_gain']=np.array(avg_gain)
+    df['avg_loss']=np.array(avg_loss)
+    df['RS'] = df['avg_gain']/df['avg_loss']
+    df['RSI'] = 100 - (100/(1+df['RS']))
+    return df['RSI']
+
+def adx(DF,n=20):
+    "function to calculate ADX"
+    df2 = DF.copy()
+    df2['H-L']=abs(df2['High']-df2['Low'])
+    df2['H-PC']=abs(df2['High']-df2['Close'].shift(1))
+    df2['L-PC']=abs(df2['Low']-df2['Close'].shift(1))
+    df2['TR']=df2[['H-L','H-PC','L-PC']].max(axis=1,skipna=False)
+    df2['+DM']=np.where((df2['High']-df2['High'].shift(1))>(df2['Low'].shift(1)-df2['Low']),df2['High']-df2['High'].shift(1),0)
+    df2['+DM']=np.where(df2['+DM']<0,0,df2['+DM'])
+    df2['-DM']=np.where((df2['Low'].shift(1)-df2['Low'])>(df2['High']-df2['High'].shift(1)),df2['Low'].shift(1)-df2['Low'],0)
+    df2['-DM']=np.where(df2['-DM']<0,0,df2['-DM'])
+
+    df2["+DMMA"]=df2['+DM'].ewm(span=n,min_periods=n).mean()
+    df2["-DMMA"]=df2['-DM'].ewm(span=n,min_periods=n).mean()
+    df2["TRMA"]=df2['TR'].ewm(span=n,min_periods=n).mean()
+
+    df2["+DI"]=100*(df2["+DMMA"]/df2["TRMA"])
+    df2["-DI"]=100*(df2["-DMMA"]/df2["TRMA"])
+    df2["DX"]=100*(abs(df2["+DI"]-df2["-DI"])/(df2["+DI"]+df2["-DI"]))
+    
+    df2["ADX"]=df2["DX"].ewm(span=n,min_periods=n).mean()
+
+    return df2['ADX']
+
+def stochOscltr(DF,a=20,b=3):
+    """function to calculate Stochastics
+       a = lookback period
+       b = moving average window for %D"""
+    df = DF.copy()
+    df['C-L'] = df['Close'] - df['Low'].rolling(a).min()
+    df['H-L'] = df['High'].rolling(a).max() - df['Low'].rolling(a).min()
+    df['%K'] = df['C-L']/df['H-L']*100
+    df['%D'] = df['%K'].ewm(span=b,min_periods=b).mean()
+    return df[['%K','%D']]
+
 ################################################################################
 ################################# main script ##################################
 ################################################################################
@@ -210,17 +368,30 @@ time.sleep(1)
 #
 
 # create a contract object
-contract = getContract(symbol="AAPL", secType="STK", currency="USD", exchange="SMART")
+#contract = getContract(symbol="AAPL", secType="STK", currency="USD", exchange="SMART")
 
 # create an order object
 # order = limitOrder(direction="BUY", quantity=1, lmt_price=200)
-order = marketOrder(direction="SELL", quantity=220)
+#order = marketOrder(direction="BUY", quantity=220)
 
 # retrieve a valid order id
-order_id = app.nextValidOrderId
+#order_id = app.nextValidOrderId
 
 # EClient function to request contract details
-app.placeOrder(order_id, contract, order)
+#app.placeOrder(order_id, contract, order)
+
+
+# get open orders
+#order_df = getOpenOrders()
+
+# get current positions
+#pos_df = getCurrentPositions()
+
+# get account summary
+#acc_df = getAccountSummary()
+
+# get profit and loss summary
+#pnl_df = getPnlSummary()
 
 
 #
